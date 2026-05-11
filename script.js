78630d6d52dc1984bb5daf937f5362d44e4d021e
SMADHAWA-Pare <tikuntuk9@gmail.com>
2026-05-11 20:30:08 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const buttons = document.querySelectorAll('.filter button');
+buttons.forEach(btn => {
+  btn.addEventListener('click', () => {
+    buttons.forEach(b => b.classList.remove('active'));
+    btn.classList.add('active');
+  });
+});
